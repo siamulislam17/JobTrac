47c24bba52e4e7e9bcdeda091fecea672df8837e
siamulislam17 <siamthca@gmail.com>
2025-05-16 00:53:32 +0600

    fixed auth problem

--- src/Auhentication/firebase.config.js
+++ src/Auhentication/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdFV9LBjtd4Xu_zxoa_TYpPmv-GuaGi14",
-  authDomain: "job-trac-3aedc.firebaseapp.com",
-  projectId: "job-trac-3aedc",
-  storageBucket: "job-trac-3aedc.firebasestorage.app",
-  messagingSenderId: "358756647732",
-  appId: "1:358756647732:web:3cbb264404dd498b1a9b61"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
